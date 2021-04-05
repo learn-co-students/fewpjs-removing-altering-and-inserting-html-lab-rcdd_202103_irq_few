1da5e425884c0dec94a2da5da668f1543f890d8b
Hasar Ali <hasar.ali@protonmail.com>
2021-04-05 13:43:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.body.removeChild(document.querySelector('main'));
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Hasar is the champion';
