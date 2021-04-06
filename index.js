3af7dd58b942d74c6ebf8733b5c86ea12e62afab
eman hamid <emanhharki@gmail.com>
2021-04-06 09:48:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML=("eman is the champion");
+// newHeader.innerText("eman is the champion");
+// newHeader.setAttribute("id","victory");
