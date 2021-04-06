0c9e8af12b493cb67728cf18387069286970861a
Daban Sadiq <dabansadiq@icloud.com>
2021-04-06 18:38:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Daban \'is the champion\' ";
+let body = document.getElementsByTagName('body')[0];
+let main = document.getElementById('main');
+main.remove();
+
+document.body.appendChild(element);
+element.style.color = 'red';
