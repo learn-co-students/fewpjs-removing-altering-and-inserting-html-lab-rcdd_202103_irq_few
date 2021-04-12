691339eead32f48ed8d5362deddcca4647a57b53
Omer Sabah <omerrj99@gmail.com>
2021-04-12 12:05:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+const newHeader=document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML='Omer is the champion'
+document.body.append(newHeader);
+
