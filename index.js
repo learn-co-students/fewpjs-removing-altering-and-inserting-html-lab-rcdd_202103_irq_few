54c84f0b3fcdd81965440eec38d941a78e4eaec5
Mariam  Khamo <mariamw736@gmail.com>
2021-05-11 11:05:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+// Create DOM elements programmatically
+// Add elements in the DOM
+// Update elements using innerHTML
+// Change properties on DOM nodes
+// Remove elements from the DOM
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
