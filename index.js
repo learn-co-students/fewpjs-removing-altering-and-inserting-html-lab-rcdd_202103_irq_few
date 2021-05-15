fcb6c2f3f482ef5388ba8c473e6dd51c06a6f7d4
Yusra Ahmed <isra.00287446@gmail.com>
2021-05-15 22:56:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main =document.getElementById("main");
+main.remove();
+
+ 
+let newHeader = document.createElement("h1");
+ 
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML="Yusra is the champion";
+document.body.appendChild(newHeader);
+ 
